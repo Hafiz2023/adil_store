6adf570a149cd59f5bc58c4b318f9f87d14c289d
Hafiz2023 <adilamin374@gmail.com>
2024-12-10 11:05:23 +0500

    update

--- src/app/HomeImage/page.tsx
+++ src/app/HomeImage/page.tsx
@@ -11,8 +11,8 @@ const HomeImage = () => {
           <Image
             src="/Man.png"
             alt="Man Banner"
-            width={2200}
-            height={2400}
+            width={2000}
+            height={2200}
             className="w-full h-auto"
           />
         </div>
